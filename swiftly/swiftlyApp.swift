a633ac32156f99dfb7a1d83cce47f85036aadd72
Luan Dang <luandang2023@gmail.com>
2026-02-03 01:36:41 -0800

    Initial Commit

---
+++ swiftly/swiftlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftlyApp.swift
+//  swiftly
+//
+//  Created by Luan Dang on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
